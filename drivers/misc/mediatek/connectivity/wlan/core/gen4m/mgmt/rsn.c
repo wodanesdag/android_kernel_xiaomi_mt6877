380140d8d623cb8d8bff77b7a8379559bcea2be8
sunyue <sunyue5@motorola.com>
2021-08-31 17:23:25 +0800

    wlan: Fix a NULL pointer issue in wlan host driver
    In a rare case, PMF wifi router sends sa query request to us before the
    connection established(aisUpdateBssInfoForJOIN), which will cause kpanic
    because prStaRecOfAP has not been set.
    
    Solution:
    Directly return from the function rsnSaQueryRequest without updating the
    MDSU_INFO_T and sending sa query response
    
    Change-Id: Ieb643f13dd1203e382881517af6cc7fb8e95c354
    Reviewed-on: https://gerrit.mot.com/2060858
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Yue Sun <sunyue5@lenovo.com>
    Reviewed-by: Bin Liu <liubin7@motorola.com>
    Submit-Approved: Jira Key
    Signed-off-by: rajdeep-3305 <rajdeepbiswas3305@gmail.com>

--- drivers/misc/mediatek/connectivity/wlan/core/gen4m/mgmt/rsn.c
+++ drivers/misc/mediatek/connectivity/wlan/core/gen4m/mgmt/rsn.c
@@ -3437,6 +3437,13 @@ void rsnApStartSaQueryTimer(IN struct ADAPTER *prAdapter,
 
 	u2PayloadLen = 2 + ACTION_SA_QUERY_TR_ID_LEN;
 
+	//BEGIN IKSWR-80158, make sure prStaRecOfAP is not null to avoid kpanic
+	if (!prBssInfo->prStaRecOfAP) {
+		DBGLOG(RSN, ERROR, "prStaRecOfAP is null\n");
+		return;
+	}
+	//END IKSWR-80158
+
 	/* 4 <3> Update information of MSDU_INFO_T */
 	TX_SET_MMPDU(prAdapter,
 		     prMsduInfo,
@@ -3593,6 +3600,13 @@ void rsnApSaQueryRequest(IN struct ADAPTER *prAdapter,
 		   ACTION_SA_QUERY_TR_ID_LEN);
 
 	u2PayloadLen = 2 + ACTION_SA_QUERY_TR_ID_LEN;
+
+	//BEGIN IKSWR-80158, make sure prStaRecOfAP is not null to avoid kpanic
+	if (!prBssInfo->prStaRecOfAP) {
+		DBGLOG(RSN, ERROR, "prStaRecOfAP is null\n");
+		return;
+	}
+	//END IKSWR-80158
 
 	/* 4 <3> Update information of MSDU_INFO_T */
 	TX_SET_MMPDU(prAdapter,
