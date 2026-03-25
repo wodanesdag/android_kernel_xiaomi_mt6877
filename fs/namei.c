9c40106cf67ec9ef86caf66aea6aa307920355b3
wodanesdag <wodanesdag@proton.me>
2026-03-25 18:20:42 +0700

    Revert "vfs: use READ_ONCE() to access ->i_link"
    This reverts commit 846bf3b84a7b41cda4c0787245f91b00052895e1.

--- fs/namei.c
+++ fs/namei.c
@@ -1163,7 +1163,7 @@ const char *get_link(struct nameidata *nd)
 		return ERR_PTR(error);
 
 	nd->last_type = LAST_BIND;
-	res = READ_ONCE(inode->i_link);
+	res = inode->i_link;
 	if (!res) {
 		const char * (*get)(struct dentry *, struct inode *,
 				struct delayed_call *);
@@ -4918,7 +4918,7 @@ int vfs_readlink(struct dentry *dentry, char __user *buffer, int buflen)
 		spin_unlock(&inode->i_lock);
 	}
 
-	link = READ_ONCE(inode->i_link);
+	link = inode->i_link;
 	if (!link) {
 		link = inode->i_op->get_link(dentry, inode, &done);
 		if (IS_ERR(link))
