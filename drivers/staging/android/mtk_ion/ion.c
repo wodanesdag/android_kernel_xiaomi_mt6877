a3ec7da387cdd75bf4a66f7a26f2085239e2abaa
rajdeep-3305 <rajdeepbiswas3305@gmail.com>
2026-03-28 19:42:20 +0000

    ion: mtk_ion: silence iommu cache sync log spam
    * remove the IONMSG calls in ion_dma_buf_begin_cpu_access and
      ion_dma_buf_end_cpu_access that fire on every CPU access to
      IOMMU/multimedia/system heap buffers
    
    Change-Id: Iec26f126f0dda0405bb37d80067d98bd191079b3
    Signed-off-by: rajdeep-3305 <rajdeepbiswas3305@gmail.com>

--- drivers/staging/android/mtk_ion/ion.c
+++ drivers/staging/android/mtk_ion/ion.c
@@ -1816,8 +1816,6 @@ static int ion_dma_buf_begin_cpu_access(struct dma_buf *dmabuf,
 
 	if (ion_iommu_heap_type(buffer) ||
 	    buffer->heap->type == (int)ION_HEAP_TYPE_SYSTEM) {
-		IONMSG("%s iommu device, to cache sync\n", __func__);
-
 		mutex_lock(&buffer->lock);
 		list_for_each_entry(a, &buffer->attachments, list) {
 			dma_sync_sg_for_cpu(a->dev,
@@ -1839,8 +1837,6 @@ static int ion_dma_buf_end_cpu_access(struct dma_buf *dmabuf,
 
 	if (ion_iommu_heap_type(buffer) ||
 	    buffer->heap->type == (int)ION_HEAP_TYPE_SYSTEM) {
-		IONMSG("%s iommu device, to cache sync\n", __func__);
-
 		mutex_lock(&buffer->lock);
 		list_for_each_entry(a, &buffer->attachments, list) {
 			dma_sync_sg_for_device(a->dev,
